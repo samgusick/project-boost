a748e47395cb65e6fe13ed26b1edad106b7e18d1
Sam Gusick <sammygusick@gmail.com>
2025-12-03 20:11:26 -0500

    init

---
+++ player.gd
@@ -0,0 +1,12 @@
+extends RigidBody3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * 1000)
+ 	
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3.BACK * 100.0 * delta)
+		
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3.FORWARD * 100.0 * delta)
